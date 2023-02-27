f2a881880a61f9db86cb6cfd3e7e1bc5f8ea3cc0
martelllipa <126447830+martelllipa@users.noreply.github.com>
2023-02-27 13:09:36 +0600

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+
+
+
+namespace Project1
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            //Read();
+            const string fileName = "bishkek_er.csv";
+
+            List<Person> people = new List<Person>();
+            List<string> lines = File.ReadAllLines(fileName).ToList();
+            foreach(var line in lines)
+            {
+                string[] parts = line.Split(',');
+
+                Person newPerson = new Person();
+
+                newPerson.Name = "@"+parts[0] + " " + parts[1];
+                newPerson.Phone =parts[2];
+                people.Add(newPerson);
+			
+				// nn
+				//part[2]
+				
+
+            }
+
+            foreach(var person in people)
+            {
+                Console.WriteLine($"{person.Name},{person.Phone}");
+            }
+
+            Console.ReadLine();
+        }
+       
+        
+           
+        }
+
+        
+
+    }
+
+    
